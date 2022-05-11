358f2b87a22de53a8d1fd56bd7d64c369a03ce92
rogermcmanley <104937013+rogermcmanley@users.noreply.github.com>
2022-05-11 20:03:52 -0300

    Add files via upload

--- questao17/questao17.c
+++ questao17/questao17.c
@@ -3,13 +3,16 @@
 
 void soma(int *v1, int *v2, int *vs, int ne){
 
-  for(int i = 0; i<ne; i++){
+  int i = 0, n;
+
+  for(i = 0; i<n; i++){
     vs[i] = v1[i] + v2[i];
   }
   }
 
 int main(void) {
   int tam;
+  int i = 0, y = 0, a = 0;
   int *vetor1,*vetor2, *vetorsoma;
 
   printf("Tamanho do vetor: ");
@@ -20,25 +23,25 @@ int main(void) {
   vetorsoma = malloc(tam*sizeof(int));
   
   // laço para gerar valores aleatórios para os vetores
-  for (int i =0; i<tam; i++){
+  for (i = 0; i<tam; i++){
     vetor1[i] = rand() % 50;
     vetor2[i] = rand() % 50;
   }
 
   printf("Vetor 1:\n");
-  for (int y =0; y<tam; y++){
+  for (y =0; y<tam; y++){
     printf("vetor 1[%d] = %d\n", y,vetor1[y]); 
   }
 
   printf("\nVetor 2:\n");
-  for (int y =0; y<tam; y++){
+  for (y =0; y<tam; y++){
     printf("vetor 2[%d] = %d\n", y,vetor2[y]); 
   }
   
   soma(vetor1,vetor2,vetorsoma,tam);
   
   printf("\nVetor soma:\n");
-  for (int a =0; a<tam; a++){
+  for (a =0; a<tam; a++){
   printf("vetorsoma[%d] = %d \n",a,vetorsoma[a]);
   }
 
