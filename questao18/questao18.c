61217e57e18f0f88ea6ca5000ddc14066a84cec8
rogermcmanley <104937013+rogermcmanley@users.noreply.github.com>
2022-05-11 20:10:00 -0300

    Add files via upload

--- questao18/questao18.c
+++ questao18/questao18.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 
 void multiplicacaomatrix(int **m1, int **m2, int **m3, int nl, int nc, int ncB){
+int i = 0, c = 0, z = 0, l = 0;
 
-for (int l = 0; l<nl; l++){
-  for (int c =0; c<nl; c++){
-    for(int z = 0; z<nc; z++){
+for (l = 0; l<nl; l++){
+  for (c =0; c<nl; c++){
+    for(z = 0; z<nc; z++){
      m3[l][c] = m1[l][z]*m2[z][c] + m3[l][c];
     }
   }
@@ -14,51 +15,55 @@ for (int l = 0; l<nl; l++){
 
 int main(void) {
   int **A,**B,**C;
+  int w = 1;
+  int i = 1;
+  int l = 0;
+  int c = 0;
   int lA = 2, cA = 3, cB = 2;
 
 A = malloc(lA*sizeof(int*));
 A[0] = malloc(cA*lA*sizeof(int));
-for (int w = 1; w<lA; w++){
+for (w = 1; w<lA; w++){
   A[w] = A[w-1] + cA;
 }
 B = malloc(cA*sizeof(int*));
 B[0] = malloc(cA*cB*sizeof(int));
-for (int i = 1; i<cA; i++){
+for (i = 1; i<cA; i++){
   B[i] = B[i-1] + cB;
 }
 C = malloc(lA*sizeof(int*));
 C[0] = malloc(lA*cB*sizeof(int));
-for (int i = 1; i<lA; i++){
+for (i = 1; i<lA; i++){
   C[i] = C[i-1] + cB;
 }
 printf("Digite um valor para matriz A:\n");
-for (int l = 0; l<lA; l++){
-  for (int c =0; c<cA; c++){
+for (l = 0; l<lA; l++){
+  for (c =0; c<cA; c++){
     scanf("%d", &A[l][c]);
   }
 }
 printf("Digite um valor para matriz B:\n");
-for (int l = 0; l<cA; l++){
-  for (int c =0; c<cB; c++){
+for (l = 0; l<cA; l++){
+  for (c =0; c<cB; c++){
     scanf("%d", &B[l][c]);
   }
 }
 printf("Digite um valor para matriz A:\n");
-for (int l = 0; l<lA; l++){
-  for (int c =0; c<cA; c++){
+for (l = 0; l<lA; l++){
+  for (c =0; c<cA; c++){
     printf("Matriz A[%d][%d] = %d\n", l+1,c+1,A[l][c]);
   }
 }
 printf("Digite um valor para matriz B:\n");
-for (int l = 0; l<cA; l++){
-  for (int c =0; c<cB; c++){
+for (l = 0; l<cA; l++){
+  for (c =0; c<cB; c++){
     printf("Matriz B[%d][%d] = %d\n", l+1,c+1,B[l][c]);
   }
 }
 multiplicacaomatrix(A, B, C, lA, cA, cB);
 printf("Matriz C:\n");
-for (int l = 0; l<lA; l++){
-  for (int c =0; c<cB; c++){
+for (l = 0; l<lA; l++){
+  for (c =0; c<cB; c++){
     printf("Matriz C[%d][%d] = %d\n", l+1,c+1,C[l][c]);
   }
 }
